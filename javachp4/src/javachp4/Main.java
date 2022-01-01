9ec9cc6ae112b2919883ba44405fc4348f4e17cb
bindu <sanjanascherry@gmail.com>
2022-01-01 12:15:42 -0500

    cleanup

--- javachp4/src/javachp4/Main.java
+++ javachp4/src/javachp4/Main.java
@@ -3,10 +3,12 @@ package javachp4;
 public class Main {
 
 	public static void main(String[] args) {
-	long miles = 	SpeedConverter.tomilesPerHour(10.5);
+
+		long miles = SpeedConverter.tomilesPerHour(10.5);
 		System.out.println(miles + " miles");
 		SpeedConverter.printConversion(10.5);
 		System.out.println("Happy New Year!");
+		System.out.println("hello");
 
 	}
 
